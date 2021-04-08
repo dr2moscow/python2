6ce293f8404c313b2423d488013736c39b427ef5
Dmitriy Rumyantsev <dr2moscow@yandex.ru>
2021-04-08 09:23:03 +0300

    Задание 4 выполнено

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -9,3 +9,32 @@
 Можете условжнить задачу, реализовав ее через ООП
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+from hashlib import sha256
+
+
+def memorize(func):
+    def get(url, memory = {}):
+        write = memory.get(sha256(url.encode('utf-8')).hexdigest())
+        if write is None:
+            write = func(url)
+            print(f'url {url} был добавлен в кеш')
+            memory[sha256(url.encode('utf-8')).hexdigest()] = write
+        else:
+            print('не добавлен, дубликат')
+        return write
+    return get
+
+
+@memorize
+def check_url(url):
+    return url
+
+
+n = 1
+
+while n == 1:
+    url_input = input('Введите url (или Enter для выхода):')
+    if url_input == "":
+        break
+    else:
+        check_url(url_input)
